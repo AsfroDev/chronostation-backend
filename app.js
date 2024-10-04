be3b640ca24c448b7815c7cfba65b744dd3013e1
Marco Dias <marcodias.bko@gmail.com>
2024-10-04 14:42:55 -0300

    fix: cors url origin

--- app.js
+++ app.js
@@ -8,16 +8,16 @@ const server = fastify()
 const port = 10000
 
 server.register(cors, {
-  origin: 'https://notes-fullstack-frontend-one.vercel.app/',
+  origin: 'https://notes-fullstack-frontend-one.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
-});
+})
 
 server.register(userRoutes)
 server.register(noteRoutes)
 server.register(groupRoutes)
 
-server.listen({ port: port || 8080, host: '0.0.0.0' }, (err, address) => {
+server.listen({ port: port, host: '0.0.0.0' }, (err, address) => {
   if (err) {
     console.error(err)
     process.exit(1)
