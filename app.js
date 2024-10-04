13ffc1d3abfdb495f6e25733d349cf9472f790ad
Marco Dias <marcodias.bko@gmail.com>
2024-10-04 14:36:16 -0300

    fix: add cors url '/'

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const server = fastify()
 const port = 10000
 
 server.register(cors, {
-  origin: 'https://notes-fullstack-frontend-one.vercel.app',
+  origin: 'https://notes-fullstack-frontend-one.vercel.app/',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 });
