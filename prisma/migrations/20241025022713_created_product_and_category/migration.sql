e7d446dbdb6046ee5676b5945f48370af2e0e1de
Marco Dias <marcodias.bko@gmail.com>
2024-10-25 03:20:15 -0300

    chore: prisma update to latest realease, schema re-design

---
+++ prisma/migrations/20241025022713_created_product_and_category/migration.sql
@@ -0,0 +1,45 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Group` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Note` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Group" DROP CONSTRAINT "Group_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Note" DROP CONSTRAINT "Note_groupId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Note" DROP CONSTRAINT "Note_userId_fkey";
+
+-- DropTable
+DROP TABLE "Group";
+
+-- DropTable
+DROP TABLE "Note";
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "categoryId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE SET NULL ON UPDATE CASCADE;
